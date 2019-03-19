6b98bb7a0491e6e1fe34ec4c062e688304249fa5
heejeongmin <hj.min1031@gmail.com>
2019-03-19 16:49:27 +0900

    20190319 houseAgent 시작

--- sabangSpring/src/main/java/com/controller/house/HouseAgentController.java
+++ sabangSpring/src/main/java/com/controller/house/HouseAgentController.java
@@ -1,8 +1,16 @@
 package com.controller.house;
 
+import javax.servlet.RequestDispatcher;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.dto.HouseInfoDTO;
+import com.dto.HouseOptionDTO;
+import com.dto.HousePriceDTO;
 import com.service.HouseService;
 
 @Controller
@@ -10,6 +18,29 @@ public class HouseAgentController {
 
 	@Autowired
 	HouseService service;
+	
+	@RequestMapping("/HouseUIControle")
+	public ModelAndView houseUIControle(@RequestParam(value="work", required=false) String work, 
+								  		@RequestParam(value="hcode", required=false) String hcode) {
+		ModelAndView mav = new ModelAndView();
+		
+		if (work != null) {
+			mav.setViewName("houseAgent");
+			mav.addObject("work", work);
+		} else if (hcode != null) {
+			mav.setViewName("houseAgent");
+			HouseInfoDTO infoDTO = service.houseRetrieve(hcode);
+			HousePriceDTO priceDTO = service.housePrice(hcode);
+			HouseOptionDTO optionDTO = service.houseOption(hcode);
+			
+			mav.addObject("infoDTO", infoDTO);
+			mav.addObject("priceDTO", priceDTO);
+			mav.addObject("optionDTO", optionDTO);
+			mav.addObject("work", "update");
+		}
+		
+		return mav;
+	}
 	
 	
 }
