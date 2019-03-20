c25e9b0ced9fe8e2aff10842b0b92a891b8c4f16
heejeongmin <hj.min1031@gmail.com>
2019-03-20 19:36:34 +0900

    1

--- sabangSpring/src/main/java/com/controller/member/MemberController.java
+++ sabangSpring/src/main/java/com/controller/member/MemberController.java
@@ -2,6 +2,7 @@ package com.controller.member;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpSession;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.dto.MemberDTO;
